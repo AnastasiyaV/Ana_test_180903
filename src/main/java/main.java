32161c3eaa36718734a7f00927fa61b98ae7a964
anastasiyavynogradska <anastasiya.vynogradska@naturalint.com>
2018-09-04 16:35:20 +0300

    first try to findElements on the page

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,13 +1,30 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 
 public class main {
+
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","./src/main/resources/chromedriver.exe");
        // System.setProperty("webdriver.chrome.driver","G:\\work\\Ana_test_180903\\src\\main\\resources\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.top10bestdatingsites.co.uk/");
+//      driver.wait.until(
+//      );
+
+
+        WebElement SiteLogoComponent = driver.findElement(By.cssSelector("[data-ni-auto-type=SiteLogoComponent]"));
+        List <WebElement> rowList = driver.findElements(By.tagName("tr"));
+       // WebElement FreeImageFeature = rowList.get(0).findElement(By.cssSelector("[data-ni-auto-type=FreeImageFeature]"));
+        for (WebElement freeImageFeature : rowList) {
+            freeImageFeature.findElement(By.cssSelector("[data-ni-auto-type=FreeImageFeature]"));
+        }
+
+
         driver.quit();
     }
 }
