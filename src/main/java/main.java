39c75170778764c20a534a43b09641051348cb29
anastasiyavynogradska <anastasiya.vynogradska@naturalint.com>
2018-09-04 17:46:21 +0300

    remove table header

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,8 +9,8 @@ import java.util.List;
 public class main {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","./src/main/resources/chromedriver.exe");
-       // System.setProperty("webdriver.chrome.driver","G:\\work\\Ana_test_180903\\src\\main\\resources\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver.exe");
+        // System.setProperty("webdriver.chrome.driver","G:\\work\\Ana_test_180903\\src\\main\\resources\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.top10bestdatingsites.co.uk/");
 //      driver.wait.until(
@@ -18,10 +18,13 @@ public class main {
 
 
         WebElement SiteLogoComponent = driver.findElement(By.cssSelector("[data-ni-auto-type=SiteLogoComponent]"));
-        List <WebElement> rowList = driver.findElements(By.tagName("tr"));
-       // WebElement FreeImageFeature = rowList.get(0).findElement(By.cssSelector("[data-ni-auto-type=FreeImageFeature]"));
+        List<WebElement> rowList = driver.findElements(By.tagName("tr"));
+//       WebElement FreeImageFeature = rowList.get(0).findElement(By.cssSelector("[data-ni-auto-type=FreeImageFeature]"));
+        //remove table header
+        rowList.remove(0);
         for (WebElement freeImageFeature : rowList) {
             freeImageFeature.findElement(By.cssSelector("[data-ni-auto-type=FreeImageFeature]"));
+            System.out.println(freeImageFeature.getTagName());
         }
 
 
